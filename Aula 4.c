e4492030c2b5855eb83bbb767de46620aeb09b90
DANILO101130 <160919866+DANILO101130@users.noreply.github.com>
2024-03-07 16:24:21 -0300

    Add files via upload

---
+++ Aula 4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "");
+	
+	int i;
+	
+	printf("Tabuada de multiplicação do número: 2 \n");
+	for (i = 5; i <= 7; i++) {
+		printf("%i x %i = %i \n", 5, i, 7*i);
+	}
+
+	printf("Tabuada de multiplicação do número: 5 \n");
+	for (i = 1; i <= 10; i++) {
+		printf("%i x %i = %i \n", 5, i, 5*i);
+	}
+	
+	
+	
+		printf("Tabuada de multiplicação do número: 7 \n");
+	for (i = 1; i <= 10; i++) {
+		printf("%i x %i = %i \n", 7, i, 7*i);
+	}
+	
+	return 0;
+	
+
+
+
+}
