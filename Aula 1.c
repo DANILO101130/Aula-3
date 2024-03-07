41a88c51e71feef816a551413a847a6f56185d3e
DANILO101130 <160919866+DANILO101130@users.noreply.github.com>
2024-03-07 16:23:42 -0300

    Add files via upload

---
+++ Aula 1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <locale.h>
+
+void verificarPositivoOuNegativo(numero) {
+	if (numero == 0) {
+		printf("Zero é neutro.");
+    } else if (numero > 0) {
+    	printf("Positivo.");
+    } else {
+    	printf("Negativo.");
+	}
+	
+}
+
+int main() {
+	setlocale(LC_ALL, "");
+	
+	int numero;
+	
+	printf("Digite um número: ");
+	scanf("%i", &numero);
+	
+	verificarPoditivoOuNegativo(numero);
+	
+	return 0;
+}
+
